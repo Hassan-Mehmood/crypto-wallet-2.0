108b1ebab8807c33bdf068892909306fe3671277
Hassan-Mehmood <hassanafridi1913@gmail.com>
2023-04-25 20:47:57 +0500

    Navbar done Working on the Highlights cards

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,58 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  Heading,
+  SimpleGrid,
+  Text,
+} from '@chakra-ui/react';
+import Navbar from './components/Navbar/Navbar';
+
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <Box px={5} mx="auto" maxW="1402px">
+      <Navbar />
+
+      <SimpleGrid spacing={4} templateColumns="repeat(auto-fill, minmax(200px, 1fr))">
+        <Card>
+          <CardHeader>
+            <Heading size="md"> Customer dashboard</Heading>
+          </CardHeader>
+          <CardBody>
+            <Text>View a summary of all your customers over the last month.</Text>
+          </CardBody>
+          <CardFooter>
+            <Button>View here</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md"> Customer dashboard</Heading>
+          </CardHeader>
+          <CardBody>
+            <Text>View a summary of all your customers over the last month.</Text>
+          </CardBody>
+          <CardFooter>
+            <Button>View here</Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader>
+            <Heading size="md"> Customer dashboard</Heading>
+          </CardHeader>
+          <CardBody>
+            <Text>View a summary of all your customers over the last month.</Text>
+          </CardBody>
+          <CardFooter>
+            <Button>View here</Button>
+          </CardFooter>
+        </Card>
+      </SimpleGrid>
+    </Box>
+  );
 }
 
 export default App;
---
+++ client/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,75 @@
+import { Box, Button, Flex, Heading } from '@chakra-ui/react';
+// import { useState } from 'react';
+// import { FaUserCircle } from 'react-icons/fa';
+// import { useNavigate } from 'react-router-dom';
+
+// import Login from '../Login/Login';
+// import Signup from '../Signup/Signup';
+// import useCheckCurrentUser from '../../Hooks/useCheckCurrentUser';
+
+const Navbar = () => {
+  // const [showSignup, setShowSignup] = useState<boolean>(false);
+  // const [showLogin, setShowLogin] = useState<boolean>(false);
+  // const navigate = useNavigate();
+
+  // const { currentUser } = useCheckCurrentUser();
+
+  // const handleSignupModal = (e: React.MouseEvent<HTMLElement>) => {
+  //   e.preventDefault();
+  //   setShowSignup(true);
+  // };
+
+  // const handleLoginModal = (e: React.MouseEvent<HTMLElement>) => {
+  //   e.preventDefault();
+  //   setShowLogin(true);
+  // };
+
+  return (
+    <Flex
+      justify="space-between"
+      align="center"
+      direction={['column', 'row']}
+      py={['1rem']}
+      minH="75px"
+    >
+      <Heading fontSize="1.4rem" mb={['0.75rem']} cursor="pointer">
+        Wallet Track
+      </Heading>
+      <Box display="flex" alignItems="center">
+        <Button
+          // onClick={(e) => handleLoginModal(e)}
+          fontSize="sm"
+          border="1px solid rgb(105, 162, 53)"
+          borderRadius="8px"
+          background="none"
+          padding={'0 16px'}
+          mr="1rem"
+          _hover={{
+            background: 'rgb(105, 162, 53)',
+            color: '#fff',
+            border: '1px solid rgb(105, 162, 53)',
+          }}
+        >
+          Log in
+        </Button>
+        <Button
+          // onClick={(e) => handleSignupModal(e)}
+          fontSize="sm"
+          borderRadius="8px"
+          color="#fff"
+          background="rgb(105, 162, 53)"
+          padding={'0 16px'}
+          border="1px solid rgb(105, 162, 53)"
+          _hover={{
+            background: 'rgb(81, 126, 39)',
+          }}
+        >
+          Sign up
+        </Button>
+      </Box>
+      {/* {showSignup && <Signup setShowLogin={setShowLogin} setShowSignup={setShowSignup} />} */}
+      {/* {showLogin && <Login setShowLogin={setShowLogin} setShowSignup={setShowSignup} />} */}
+    </Flex>
+  );
+};
+export default Navbar;
