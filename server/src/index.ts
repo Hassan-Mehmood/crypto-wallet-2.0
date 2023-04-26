1e2f0047a996ba91a81b2908555711b5300a7cd0
Hassan Mehmood <hassanafridi1913@gmail.com>
2023-04-26 13:25:27 +0500

    working on highlights route

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,30 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import axios from 'axios';
 
 const app = express();
-const port = 3000;
+const port = 3001;
+
+const instance = axios.create({
+  baseURL: 'https://pro-api.coinmarketcap.com/v1',
+  headers: {
+    'X-CMC_PRO_API_KEY': '872c34b0-d2d7-471b-9897-83590cd3c996',
+  },
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+});
+
+app.get('/highlights', async (req: Request, res: Response) => {
+  // latest listing:      https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/new
+  // Gainers and losers:  https://pro-api.coinmarketcap.com/v1/cryptocurrency/trending/gainers-losers
+  // Trending:            https://pro-api.coinmarketcap.com/v1/cryptocurrency/trending/latest
+
+  const data = await instance.get('/cryptocurrency/listings/new');
+
+  console.log(data);
+
+  // instance.get('/cryptocurrency/trending/gainers-losers');
 });
 
 app.listen(port, () => {
