2860c4d5228cbb3e9b4a6e08eddafacaa08c022e
Hassan-Mehmood <hassanafridi1913@gmail.com>
2023-04-28 20:26:17 +0500

    Created login form

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -2,7 +2,13 @@ import {
   Box,
   Button,
   Flex,
+  FormControl,
+  FormHelperText,
+  FormLabel,
   Heading,
+  Input,
+  InputGroup,
+  InputRightElement,
   Modal,
   ModalBody,
   ModalCloseButton,
@@ -12,17 +18,71 @@ import {
   ModalOverlay,
   useDisclosure,
 } from '@chakra-ui/react';
+import { useState } from 'react';
+import { Eye, EyeOff } from 'react-feather';
+
+function UserForm() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <form>
+      <FormControl>
+        <FormLabel fontWeight="semibold">Name</FormLabel>
+        <Input type="text" placeholder="Enter your name" />
+      </FormControl>
+
+      <FormControl mt={4}>
+        <FormLabel fontWeight="semibold">Email address</FormLabel>
+        <Input type="email" placeholder="Enter your email address" />
+      </FormControl>
+
+      <FormControl mt={4}>
+        <FormLabel fontWeight="semibold">Password</FormLabel>
+        <InputGroup>
+          <Input type={showPassword ? 'text' : 'password'} placeholder="Enter your password" />
+          <InputRightElement mr={1}>
+            <Button h="1.75rem" size="sm" onClick={togglePasswordVisibility}>
+              {showPassword ? <Eye /> : <EyeOff />}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </FormControl>
+    </form>
+  );
+}
 
 function LoginModal({ isOpen, onClose }: any) {
   return (
     <>
-      <Modal isOpen={isOpen} onClose={onClose}>
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
         <ModalOverlay />
         <ModalContent>
-          <ModalHeader>Modal Title</ModalHeader>
+          <ModalHeader>Login</ModalHeader>
           <ModalCloseButton />
-          <ModalBody>Hello</ModalBody>
+          <ModalBody>
+            <UserForm />
+          </ModalBody>
 
+          <ModalFooter></ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
+
+const SignupModal = ({ isOpen, onClose }: any) => {
+  return (
+    <>
+      <Modal isOpen={isOpen} onClose={onClose} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Sign up</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>Signup</ModalBody>
           <ModalFooter>
             <Button colorScheme="blue" mr={3} onClick={onClose}>
               Close
@@ -32,10 +92,11 @@ function LoginModal({ isOpen, onClose }: any) {
       </Modal>
     </>
   );
-}
+};
 
 const Navbar = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { isOpen: isLoginOpen, onOpen: onLoginOpen, onClose: onLoginClose } = useDisclosure();
+  const { isOpen: isSignupOpen, onOpen: onSignupOpen, onClose: onSignupClose } = useDisclosure();
 
   return (
     <nav>
@@ -51,7 +112,7 @@ const Navbar = () => {
         </Heading>
         <Box display="flex" alignItems="center">
           <Button
-            onClick={onOpen}
+            onClick={onLoginOpen}
             fontSize="sm"
             border="1px solid rgb(105, 162, 53)"
             borderRadius="8px"
@@ -67,6 +128,7 @@ const Navbar = () => {
             Log in
           </Button>
           <Button
+            onClick={onSignupOpen}
             fontSize="sm"
             borderRadius="8px"
             color="#fff"
@@ -81,7 +143,8 @@ const Navbar = () => {
           </Button>
         </Box>
       </Flex>
-      <LoginModal isOpen={isOpen} onClose={onClose} />
+      <LoginModal isOpen={isLoginOpen} onClose={onLoginClose} />
+      <SignupModal isOpen={isSignupOpen} onClose={onSignupClose} />
     </nav>
   );
 };
