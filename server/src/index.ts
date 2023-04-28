219a2e9ed558ab9ed8d91023fd6ae0b074b2ee43
Hassan Mehmood <hassanafridi1913@gmail.com>
2023-04-28 15:11:34 +0500

    add user to database done

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,41 @@
 import express, { Request, Response } from 'express';
-import { PrismaClient } from '@prisma/client';
+import bodyParser from 'body-parser';
+import { body, validationResult } from 'express-validator';
+
+import { prisma } from './utils/client';
 
 const app = express();
 const port = 3001;
 
-const prisma = new PrismaClient();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', async (req, res) => {});
+app.get('/', async (req, res) => res.send('Hello World!'));
 
-app.get('/users', async (req: Request, res: Response) => {});
+app.post(
+  '/api/create-user',
+
+  body('email').isEmail().normalizeEmail().withMessage('Invalid email'),
+  body('password').isLength({ min: 6 }).withMessage('Password must be at least 6 characters'),
+  body('name').notEmpty().withMessage('Name is required'),
+
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const { name, email, password } = req.body;
+      const user = await prisma.user.create({
+        data: { name, email, password },
+      });
+      return res.status(201).json(user);
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+  }
+);
 
 app.listen(port, () => {
   console.log(`App listening on port http://localhost:${port}`);
