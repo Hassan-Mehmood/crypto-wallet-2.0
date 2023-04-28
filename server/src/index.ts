0e5c0165c3759c21171c7b9715a8da694224fe39
Hassan Mehmood <hassanafridi1913@gmail.com>
2023-04-28 15:28:08 +0500

    added endpoint to get single user

--- server/src/index.ts
+++ server/src/index.ts
@@ -37,6 +37,20 @@ app.post(
   }
 );
 
+app.get('/api/user/:id', async (req: Request, res: Response) => {
+  try {
+    const id: number = parseInt(req.params.id);
+
+    const user = await prisma.user.findUnique({ where: { id } });
+
+    if (!user) return res.status(404).json({ error: 'User not found' });
+
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
 app.listen(port, () => {
   console.log(`App listening on port http://localhost:${port}`);
 });
