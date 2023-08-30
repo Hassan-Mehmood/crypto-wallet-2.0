96ba25b43621b92ae123ca43229b1d307c52b65e
Hamza Ali <ellawliet1007@gmail.com>
2023-08-30 13:08:38 -0700

    started working on highlights

--- client/src/components/Home/highlights/Highlights.tsx
+++ client/src/components/Home/highlights/Highlights.tsx
@@ -1,5 +1,4 @@
-import { SimpleGrid, Card, CardHeader, Heading, CardBody, Spinner } from '@chakra-ui/react';
-import React from 'react';
+import { SimpleGrid, Card, CardHeader, Heading, CardBody, Spinner, GridItem } from '@chakra-ui/react';
 import GlobalData from './GlobalData';
 import MarketDominance from './MarketDominance';
 import TrendingCoins from './TrendingCoins';
@@ -12,8 +11,8 @@ export default function Highlights() {
   const globalData = data && data[1].data;
 
   return (
-    <SimpleGrid spacing={4} templateColumns="repeat(3, 1fr)">
-      <Card >
+    <SimpleGrid spacing={4} templateColumns={["repeat(2, 1fr)", "repeat(2, 1fr)", "repeat(3, 1fr)"]}>
+      <Card>
         <CardHeader>
           <Heading size="md">Trending Coins</Heading>
         </CardHeader>
@@ -40,14 +39,16 @@ export default function Highlights() {
           />
         </CardBody>
       </Card>
-      <Card>
-        <CardHeader>
-          <Heading size="md">Market Dominance</Heading>
-        </CardHeader>
-        <CardBody>
-          <MarketDominance dominance={globalData?.market_cap_percentage} isLoading={isLoading} />
-        </CardBody>
-      </Card>
+      <GridItem colSpan={[2, 2, 1]} display={["flex", "flex", "block"]} justifyContent={"center"}>
+        <Card width={["43.2vw", "43.2vw", "full"]}>
+          <CardHeader>
+            <Heading size="md">Market Dominance</Heading>
+          </CardHeader>
+          <CardBody>
+            <MarketDominance dominance={globalData?.market_cap_percentage} isLoading={isLoading} />
+          </CardBody>
+        </Card>
+      </GridItem>
     </SimpleGrid>
   );
 }
