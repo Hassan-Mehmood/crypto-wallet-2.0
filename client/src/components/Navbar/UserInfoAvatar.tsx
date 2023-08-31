80a95e4c0c7b7bce8b1312bf8882563668cf3773
Hamza Ali <ellawliet1007@gmail.com>
2023-08-31 11:15:08 -0700

    Refactored the Navigation when user is logged in

--- client/src/components/Navbar/UserInfoAvatar.tsx
+++ client/src/components/Navbar/UserInfoAvatar.tsx
@@ -1,5 +1,8 @@
-import { Menu, MenuButton, MenuList, MenuItem, Text } from '@chakra-ui/react';
-import { User } from 'react-feather';
+import { Menu, MenuButton, MenuList, MenuItem, Text, Icon, Box, Flex } from '@chakra-ui/react';
+import { BiBitcoin } from "react-icons/bi";
+import { AiOutlineProfile } from "react-icons/ai"
+import { FiLogOut } from "react-icons/fi"
+import { FaRegCircleUser } from "react-icons/fa6";
 import { Link } from 'react-router-dom';
 import { UserState, logout } from '../../slices/userSlice';
 import { useDispatch } from 'react-redux';
@@ -21,46 +24,58 @@ export default function UserInfoAvatar({ user }: props) {
   }
 
   return (
-    <>
-      <Menu>
-        <MenuButton bg="rgba(0, 0, 0, 0.2)" p={3} borderRadius="50%">
-          <User size={24} />
-        </MenuButton>
-        <MenuList p={0}>
-          <MenuItem _hover={{ backgroundColor: 'rgba(0, 0, 0,0.1)' }}>
-            <Text minW="100%" py="0.5rem">
-              {user.name}
-            </Text>
-          </MenuItem>
-          <MenuItem _hover={{ backgroundColor: 'rgba(0, 0, 0,0.1)' }}>
-            <Text minW="100%" py="0.5rem">
-              {user.email}
-            </Text>
-          </MenuItem>
-          <Link to={'/addcoin'}>
-            <MenuItem _hover={{ backgroundColor: 'rgba(0, 0, 0,0.1)' }}>
-              <Text minW="100%" py="0.5rem">
-                Add Coins
-              </Text>
+      <Menu >
+          <MenuButton bg="#8bc53f" borderRadius="100%" color={"#fff"} width={["2.5rem", "2.5rem", "2.8rem"]} height={["2.5rem", "2.5rem", "2.8rem"]}>
+            <Box display={"flex"} justifyContent={"center"} alignItems={"center"}>
+              <Icon as={FaRegCircleUser} fontSize={["1.3rem", "1.5rem"]} />
+            </Box>
+          </MenuButton>
+          <MenuList p={0} borderRadius={"0.5rem"}>
+            <MenuItem _hover={{ backgroundColor: 'rgba(0, 0, 0,0.1)', borderTopRadius: "0.5rem" }} >
+              <Box width={"full"}>
+                <Text fontWeight={"semibold"} minW="100%" py="0.5rem" display={"flex"} justifyContent={"center"}>
+                  {user.name}
+                </Text>
+              </Box>
             </MenuItem>
-          </Link>
-          <Link to={'/portfolio'}>
-            <MenuItem _hover={{ backgroundColor: 'rgba(0, 0, 0,0.1)' }}>
-              <Text minW="100%" py="0.5rem">
-                Portfolio
-              </Text>
-            </MenuItem>
-          </Link>
-          <MenuItem
-            _hover={{ backgroundColor: 'rgba(0, 0, 0,0.1)' }}
-            onClick={() => dispatch(logout())}
-          >
-            <Text minW="100%" py="0.5rem" onClick={handleLogout}>
-              Logout
+            {/* <MenuItem _hover={{ backgroundColor: 'rgba(0, 0, 0,0.1)' }}>
+            <Text minW="100%" py="0.5rem">
+            {user.email}
             </Text>
-          </MenuItem>
-        </MenuList>
+          </MenuItem> */}
+            <Link to={'/addcoin'}>
+              <MenuItem _hover={{ backgroundColor: 'rgba(0, 0, 0,0.1)' }}>
+                <Flex alignItems={"center"}>
+                  <Icon as={BiBitcoin} mr={"1rem"} />
+                  <Text minW="100%" py="0.5rem">
+                    Add Coins
+                  </Text>
+                </Flex>
+              </MenuItem>
+            </Link>
+            <Link to={'/portfolio'}>
+              <MenuItem _hover={{ backgroundColor: 'rgba(0, 0, 0,0.1)' }}>
+                <Flex alignItems={"center"}>
+                  <Icon as={AiOutlineProfile} mr={"1rem"} />
+                  <Text minW="100%" py="0.5rem">
+                    Portfolio
+                  </Text>
+                </Flex>
+              </MenuItem>
+            </Link>
+            <MenuItem
+              _hover={{ backgroundColor: 'rgba(0, 0, 0,0.1)', borderBottomRadius: "0.5rem" }}
+              onClick={() => dispatch(logout())}
+            >
+              <Flex alignItems={"center"}>
+                <Icon as={FiLogOut} mr={"1rem"} />
+                <Text minW="100%" py="0.5rem" onClick={handleLogout}>
+                  Logout
+                </Text>
+              </Flex>
+
+            </MenuItem>
+          </MenuList>
       </Menu>
-    </>
   );
 }
